4e762882baee1ab09b57f39e5cb83e2b88d86ee8
AlisiaSmith <anya0192837465@ya.ru>
2018-10-04 16:04:52 +0700

    something about insert

--- hash/Classes.cpp
+++ hash/Classes.cpp
@@ -7,7 +7,7 @@ HashTable::HashTable(const HashTable& b)
 
 void HashTable::swap(HashTable& b)
 {
-	
+
 }
 
 HashTable& HashTable::operator=(const HashTable& b)
@@ -28,6 +28,18 @@ bool HashTable::erase(const Key& k)
 
 bool HashTable::insert(const Key& k, const Value& v)
 {
+	if (used == quantity) recount;
+	if(contains(k)) return FALSE;
+	int hash = hash_count(k);
+	while (TRUE)
+	{
+		if(!list[(hash)%quantity])
+		{
+			list[(hash)%quantity] = v;
+			break;
+		}
+		hash++;
+	}
 
 }
 
